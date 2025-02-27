2a5a21b2ad1167b1a9c58a4728671c95111f2970
Lotl_Ax <19cpetleya@hbk.acesmat.uk>
2025-02-27 16:40:36 +0000

    Coded to find hightest and lowest latitude and longitude values. Created notes for next stage regarding regions and their assigned boundaries

--- testjeson.py
+++ testjeson.py
@@ -1,32 +1,60 @@
 import json
 
-'''
-month = input(str('Enter a month: ')).lower()
-year = input(str('Enter a Year: '))
+#  getting largest and smallest longitudes and latitudes
+#{item['location']}
 
-months = {
-    'january':1,
-    'february':2,
-    'march':3,
-    'april':4,
-    'may':5,
-    'june':6,
-    'july':7,
-    'august':8,
-    'september':9,
-    'october':10,
-    'november':11,
-    'december':12
-}
-monthint = months[month]
-date = f'{year}-0{monthint}'
-print(date)
-'''
+'''lat_min = '51.0'
+lat_max = '51.0'
+long_min = '1.0'
+long_max = '1.0'
 
 with open ("output.json", "r") as cf:
     ojson = json.load(cf)
 
     for item in ojson['crimes']:
-        if item['crimeType'] != 'Burglary':
-            if item['location'] != 'No Location':
-                print(f'{item['location']}, {item['LSOACode']}, {item['LSOAName']}')
+
+        if item['latitude'] != '' and item['longitude'] != '':
+
+            cur_lat = item['latitude']
+            cur_long = item['longitude']
+
+            if cur_lat < lat_min:
+                lat_min = cur_lat
+
+            if cur_lat > lat_max:
+                lat_max = cur_lat
+
+            if cur_long < long_min:
+                long_min = cur_long
+
+            if cur_long > long_max:
+                long_max = cur_long
+        
+
+            
+
+print(f'lowest latitude: {lat_min}')
+print(f'highest latitude: {lat_max}')
+print(f'lowest longitude: {long_min}')
+print(f'highest longitude: {long_max}')
+'''
+
+low_lat = 50.86193
+high_lat = 54.951083
+low_long = -0.000358
+high_long = 1.269397
+
+lat_dif = high_lat - low_lat
+long_dif = high_long - low_long
+
+#all left regions starting long = low_long
+#all right regions ending long = high_long
+#all top regions starting lat = high_lat
+#all bottom regions ending lat = low_lat
+
+#div lat_dif by 3 and create each row latitude boundary
+#div long_dif by 4 and create each columns longitude boundary
+
+# assign each region their boundaries
+
+#combine set up and this
