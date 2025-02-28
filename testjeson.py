651e42332417facaedd559e6d5ef86ec07ef617e
Lotl_Ax <19cpetleya@hbk.acesmat.uk>
2025-02-28 11:52:35 +0000

    added region boundaries, started to fetch data and sort into regions

--- testjeson.py
+++ testjeson.py
@@ -46,15 +46,123 @@ high_long = 1.269397
 
 lat_dif = high_lat - low_lat
 long_dif = high_long - low_long
+print(f'{lat_dif} \n{long_dif}')
 
-#all left regions starting long = low_long
-#all right regions ending long = high_long
-#all top regions starting lat = high_lat
-#all bottom regions ending lat = low_lat
+#all left regions (1,2,3) starting long = low_long
+#all right regions (10, 11, 12) ending long = high_long
+#all top regions (1,4,7,11)starting lat = high_lat
+#all bottom regions (3,6,9,12)ending lat = low_lat
 
 #div lat_dif by 3 and create each row latitude boundary
+lat_divd = lat_dif/3
+print(lat_divd)
 #div long_dif by 4 and create each columns longitude boundary
+long_divd = long_dif/4
+print(long_divd)
+
 
 # assign each region their boundaries
 
+Region_boundaries = {
+    'Region_1_boundaries' : {
+        'top':high_lat,
+        'bottom':high_lat-lat_divd,
+        'left':low_long,
+        'right':low_long+long_divd
+    } ,
+
+    'Region_2_boundaries' : {
+        'top':high_lat-lat_divd,
+        'bottom':low_lat + lat_divd,
+        'left':low_long,
+        'right':low_long+long_divd
+    } ,
+
+    'Region_3_boundaries' : {
+        'top':low_lat + lat_divd,
+        'bottom':low_lat,
+        'left':low_long,
+        'right':low_long+long_divd
+    },
+
+    'Region_4_boundaries' : {
+        'top':high_lat,
+        'bottom':high_lat-lat_divd,
+        'left':low_long+long_divd,
+        'right':low_long+2(long_divd)
+    } ,
+
+    'Region_5_boundaries': {
+        'top':high_lat-lat_divd,
+        'bottom':low_lat + lat_divd,
+        'left':low_long+long_divd,
+        'right':low_long+2(long_divd)
+    },
+
+    'Region_6_boundaries' : {
+        'top':low_lat + lat_divd,
+        'bottom':low_lat,
+        'left':low_long+long_divd,
+        'right':low_long+2(long_divd)
+    },
+
+    'Region_7_boundaries' : {
+        'top':high_lat,
+        'bottom':high_lat-lat_divd,
+        'left':high_long-2(long_divd),
+        'right':high_long-long_divd
+    },
+
+    'Region_8_boundaries' : {
+        'top':high_lat-lat_divd,
+        'bottom':low_lat + lat_divd,
+        'left':high_long-2(long_divd),
+        'right':high_long-long_divd
+    },
+
+    'Region_9_boundaries' : {
+        'top':low_lat + lat_divd,
+        'bottom':low_lat,
+        'left':high_long-2(long_divd),
+        'right':high_long-long_divd
+    },
+
+    'Region_10_boundaries' : {
+        'top':high_lat,
+        'bottom':high_lat-lat_divd,
+        'left':high_long-(long_divd),
+        'right':high_long
+    },
+
+    'Region_11_boundaries' : {
+        'top':high_lat-lat_divd,
+        'bottom':low_lat + lat_divd,
+        'left':high_long-(long_divd),
+        'right':high_long
+    },
+
+    'Region_12_boundaries' : {
+        'top':low_lat + lat_divd,
+        'bottom':low_lat,
+        'left':high_long-(long_divd),
+        'right':high_long
+    }
+}
 #combine set up and this
+
+
+
+#adapt following for region assigning
+
+with open ("output.json", "r") as cf:   #  loading JSON file
+      ojson = json.load(cf)
+
+      for item in ojson['crimes']:
+          
+          if item['latitu'] in crimelist: # only enters the next line if the crimeType of the current item matches one of the crimes in the crime list
+              
+              if item['location'] != 'No Location': # wont continue if there is no location for the cirime
+                  
+                  crimeCounter[item['crimeType']] += 1 #  increases the number associated with the crimeType of the current item by one
+                  print(f'{item['crimeType']}, {item['location']}, {item['LSOACode']}, {item['LSOAName']}')
+
