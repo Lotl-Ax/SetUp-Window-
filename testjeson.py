c7ea2b5dcef3d1a30f8f70666e808a4b4b9daa7d
Lotl_Ax <19cpetleya@hbk.acesmat.uk>
2025-03-04 09:05:04 +0000

    removed comments

--- testjeson.py
+++ testjeson.py
@@ -4,46 +4,6 @@ from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QCheckBox, QPush
 
 import sys
 
-
-#  getting largest and smallest longitudes and latitudes
-#{item['location']}
-
-'''lat_min = '51.0'
-lat_max = '51.0'
-long_min = '1.0'
-long_max = '1.0'
-
-with open ("output.json", "r") as cf:
-    ojson = json.load(cf)
-
-    for item in ojson['crimes']:
-
-        if item['latitude'] != '' and item['longitude'] != '':
-
-            cur_lat = item['latitude']
-            cur_long = item['longitude']
-
-            if cur_lat < lat_min:
-                lat_min = cur_lat
-
-            if cur_lat > lat_max:
-                lat_max = cur_lat
-
-            if cur_long < long_min:
-                long_min = cur_long
-
-            if cur_long > long_max:
-                long_max = cur_long
-        
-
-            
-
-print(f'lowest latitude: {lat_min}')
-print(f'highest latitude: {lat_max}')
-print(f'lowest longitude: {long_min}')
-print(f'highest longitude: {long_max}')
-'''
-
 low_lat = 50.86193
 high_lat = 54.951083
 low_long = -0.000358
