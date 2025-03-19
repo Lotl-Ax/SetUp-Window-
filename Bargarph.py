7b827eb5b952efd453e7974b6a8ee2704a73af6b
Lotl_Ax <19cpetleya@hbk.acesmat.uk>
2025-03-19 10:40:31 +0000

    Worked on creating bar graphs (error regarding all crimes being at the same value)

--- Bargarph.py
+++ Bargarph.py
@@ -1,13 +1,31 @@
 import matplotlib.pyplot as Mplt
+import json
 
-crime = ['Antisocial behaviour', 'Burglary', 'Criminal Damage and Arson', 'Drugs']
-amount = [200, 50, 150, 75]
+crime = ['Antisocial behaviour', 'Bicycle Theft', 'Burglary', 'Criminal Damage', 'Drugs', 'Possession of Weapons', 'Public Order', 'Robbery', 'Shoplifting', 'Theft from Person', 'Vehicle Crime', 'Violence and Sexual offences']
+amount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+with open ("output.json", "r") as cf:   #  loading JSON file
+    ojson = json.load(cf)
+    
+    for item in ojson['crimes']:
+          
+        for item['crimeType'] in crime: 
+            index = crime.index(item['crimeType'])
+            amount[index] += 1
+
+
+
+
+
+font = {'family':'serif','color':'darkred','size':10}
 
 Mplt.barh(crime, amount, height=0.5, color=['blue', 'green'])
 
-Mplt.ylabel('Crime')
-Mplt.xlabel('Amount')
+Mplt.ylabel('Crime', fontdict=font)
+Mplt.xlabel('Amount', fontdict=font)
 
 Mplt.title('Crime amounts')
+
+Mplt.tight_layout()
 
 Mplt.show()
--- testjeson.py
+++ testjeson.py
@@ -4,25 +4,6 @@ from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QCheckBox, QPush
 
 import sys
 
-low_lat = 50.86193
-high_lat = 54.951083
-low_long = -0.000358
-high_long = 1.269397
+test = [3, 7, 5, 9, 1, 6, 82, 3,73,38]
 
-lat_dif = high_lat - low_lat
-long_dif = high_long - low_long
-
-mid_lat_dif = lat_dif/2
-centre_lat = low_lat+mid_lat_dif
-
-mid_long_dif = long_dif/2
-centre_long = low_long + mid_long_dif
-
-print(f'centre: {centre_lat},{centre_long}')
-print(f'topleft: {high_lat},{low_long}')
-print(f'bttmRght: {low_lat},{high_long}')
-
-
-
-#52.9065065
-#0.6345195000000001
+print(test[5])
