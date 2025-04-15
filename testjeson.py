c0b4900bc28fee8ce477c9de89926edc4c43e2ff
Lotl_Ax <19cpetleya@hbk.acesmat.uk>
2025-04-15 15:18:24 +0100

    reminded myself how dictionaries and lists work together so i could use in region assignment

--- testjeson.py
+++ testjeson.py
@@ -1,10 +1,30 @@
-import json
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QCheckBox, QPushButton, QMessageBox, QComboBox, QGraphicsView
+test = {
+    'a': {'Id':['Tis ID'],
+          'Longitude':['Tis Loing'],
+          'Latitude':['Tis LAt']
+          },
+    'b': []
+}
 
-import sys
+print(test['a']['Id'])
+print(test['a'])
+a = test['a']
+id = test['a']['Id']
+print(type(a))
+print(type(id))
 
-test = [3, 7, 5, 9, 1, 6, 82, 3,73,38]
+Test2 = {
+    '1': [('Id1', 'Long1', 'Lat1'), ('Id2', 'Ling2', 'Lat2')],
+    '2':'hi'
+}
 
-index = test.index(5)
-print(index)
+print(Test2['1'])
+print(type(Test2['1']))
+check = Test2['1']
+print(check[1])
+
+new_info = ('Id3', 'Long3', 'Lat3')
+check.append(new_info)
+print(check)
+
+print(check[1][1])
